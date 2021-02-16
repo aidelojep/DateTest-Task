a988cc0ed66ea4fe51d853f50c5f9168094580b8
aidelojep <57565189+aidelojep@users.noreply.github.com>
2021-02-16 22:38:14 +0100

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,62 @@
+public class Date {
+
+private int month;
+private int day;
+private int year;
+
+public Date (int day, int month, int year)
+{
+this.month=month;
+
+this.day=day;
+  
+this.year= year;
+
+}
+
+
+
+ public void setMonth(int month)
+{
+    this.month = month;
+}
+
+public int getMonth() {
+ return month;}
+
+
+
+public void setDay (int day) {
+     this.day = day;
+}
+
+public int getDay() { 
+return day
+;}
+
+
+
+public void setYear (int year) {
+  this.year = year;
+
+}
+
+public int getYear() {
+  return year
+
+;}
+
+
+
+
+public String displayDate() {
+return (month + "/" + day + "/" + year);
+
+
+}
+
+
+
+
+
+}
---
+++ DateTest.java
@@ -0,0 +1,33 @@
+public class DateTest {
+   public static void main (String [] args) {
+
+    Date myDate = new Date(04 , 20, 1994);
+
+      myDate.setDay(20);
+      myDate.setMonth(03);
+      myDate.setYear(1994);
+
+   
+System.out.print("The correct value of date is " + myDate.displayDate());
+    
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
